4ef7b6540610d294d163b8d75b805c002c6822bc
Nguyễn Thị Lan Anh <nguyenthilananh.it@gmail.com>
2026-09-18 14:29:35 +0700

    Add files via upload

---
+++ cuago/cms-config.js
@@ -0,0 +1,6 @@
+/* Huỳnh Gia Door CMS configuration */
+window.HGD_CMS_API = '';
+window.HGD_GOOGLE_MAP_URL = 'https://share.google/fv93hf1bNYympQqq8';
+window.HGD_DRIVE_ROOT = '1l4gQZ6V7BJwHkbadjMvn_wgxD-eBrTYC';
+window.HGD_DRIVE_HERO = '1gHpgwO2xCEarWXW2dOzEo_j8wGMYEw3G';
+window.HGD_HERO_IMAGE = 'assets/nha-dep-tu-cua.svg';
